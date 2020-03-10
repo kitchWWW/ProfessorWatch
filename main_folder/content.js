e2d9165d40b0333d223d6bb464fb3931578d498e
kitchWWW <brian.e2014@gmail.com>
2020-03-10 08:08:17 -0700

    updated list and icon

--- main_folder/content.js
+++ main_folder/content.js
@@ -8,6 +8,12 @@ if(myLoc.startsWith("https://utdirect.utexas.edu/apps")){
 	  'HOFMANN, J': 'https://www.scribd.com/document/442326249/OIE-Report-Findings-of-Violations-of-HOP-3-3031',
 	  'DALBY, KEVIN N':'https://www.scribd.com/document/442326249/OIE-Report-Findings-of-Violations-of-HOP-3-3031',
 	  'DALBY, K':'https://www.scribd.com/document/442326249/OIE-Report-Findings-of-Violations-of-HOP-3-3031',
+	  'HUBBARD, T': 'https://thetexan.news/ut-reviewing-concerns-about-professor-who-writes-on-pederasty/',
+	  'HUBBARD, THOMAS K': 'https://thetexan.news/ut-reviewing-concerns-about-professor-who-writes-on-pederasty/',
+	  'SARKAR, S':'https://www.statesman.com/news/20180819/at-ut-an-inappropriate-relationship-alters-futures-raises-questions',
+	  'SARKAR, SAHOTRA':'https://www.statesman.com/news/20180819/at-ut-an-inappropriate-relationship-alters-futures-raises-questions',
+	  'REECE, ROBERT L': 'https://www.vox.com/2018/4/17/17215554/metoo-movement-robert-reece-ut-austin-sexual-misconduct',
+	  'REECE, R' : 'https://www.vox.com/2018/4/17/17215554/metoo-movement-robert-reece-ut-austin-sexual-misconduct',
 	};
 
 	var tribuneURL = "https://www.texastribune.org/2020/01/09/ut-austin-acknowledges-17-cases-sexual-misconduct-employees/";
